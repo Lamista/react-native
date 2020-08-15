0c8b2bf7cc53d398a981009f3cc2b95db2820bf8
Laura <laura.stasiule@gmail.com>
2020-08-15 22:53:56 +0300

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+import { ScrollView } from 'react-native-gesture-handler';
 
 class Contact extends Component {
 
@@ -10,14 +12,19 @@ class Contact extends Component {
 
     render() {
         return (
-            <Card title="Contact Information" >
-                <Text style={{ margin: 10 }}>121, Clear Water Bay Road{'\n\n'}
+            <ScrollView>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                    <Card
+                        title='Contact Information'>
+                        <Text style={{ margin: 10 }}>121, Clear Water Bay Road{'\n\n'}
                 Clear Water Bay, Kowloon{'\n\n'}
                 HONG KONG{'\n\n'}
                 Tel: +852 1234 5678{'\n\n'}
                 Fax: +852 8765 4321{'\n\n'}
                 Email:confusion@food.net</Text>
-            </Card>
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
         )
     }
 }
